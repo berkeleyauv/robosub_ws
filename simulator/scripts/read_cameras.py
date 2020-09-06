97966888bf8ef45676b01a011b690c7d15b624d5
Marco Gellecanao <marcogelle@berkeley.edu>
2020-09-06 14:04:13 -0700

    added bottom camera to image listener script

--- simulator/scripts/read_cameras.py
+++ simulator/scripts/read_cameras.py
@@ -2,13 +2,9 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-class ImageListener():
+class ImageListener:
     """ Subscribes to ROS Image messages. Used for getting camera image data
     from the simulator and converting it for OpenCV. """
-
-    def __init__(self):
-        self.sub = rospy.Subscriber("/urab_sub/urab_sub/camerafront/camera_image",
-            Image, self.callback)
 
     def callback(self, img_msg):
         bridge = CvBridge()
@@ -21,11 +17,35 @@ class ImageListener():
         rospy.sleep(time) # time is in seconds
         return self.images
 
+class FrontCameraListener(ImageListener):
+    """ Gets image data from the front camera. """
+    def __init__(self):
+        self.subscr = rospy.Subscriber("/urab_sub/urab_sub/camerafront/camera_image",
+            Image, self.callback)
+
+class UnderCameraListener(ImageListener):
+    """ Gets image data from the bottom camera. """
+    def __init__(self):
+        self.subscr = rospy.Subscriber("/urab_sub/urab_sub/cameraunder/camera_image",
+            Image, self.callback)
+
 if __name__ == '__main__':
     rospy.init_node('img_listener', anonymous=True)
-    listener = ImageListener()
-    imgs = listener.get_images(1.0)
+    front_listen = FrontCameraListener()
+    imgs = front_listen.get_images(1.0)
 
+    print "FRONT CAMERA"
+    print "ImageListener.get_images() has type " + str(type(imgs))
+    print "Length of image list: " + str(len(imgs))
+    print "The first image has type " + str(type(imgs[0]))
+    print "The first image has dimensions " + str(imgs[0].shape)
+    print "Print out the first image:"
+    print imgs[0]
+
+    under_listen = UnderCameraListener()
+    imgs = under_listen.get_images(1.0)
+
+    print "\n\nBOTTOM CAMERA"
     print "ImageListener.get_images() has type " + str(type(imgs))
     print "Length of image list: " + str(len(imgs))
     print "The first image has type " + str(type(imgs[0]))
