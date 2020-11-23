3cfb98017072cb5fa9a2af308fd1af01b2cb1bd7
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-11-22 17:57:41 -0800

    Added XML and YAMl launch file support. Added Gazebo sourcing

--- scripts/install_scripts/install-ros-eloquent.sh
+++ scripts/install_scripts/install-ros-eloquent.sh
@@ -9,8 +9,9 @@ curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo ap
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 
 sudo apt update -y
-sudo apt install -y ros-eloquent-desktop
+sudo apt install -y ros-eloquent-desktop ros-eloquent-launch-xml ros-eloquent-launch-yaml
 echo "source /opt/ros/eloquent/setup.bash" >> ~/.bashrc
+echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install -y -qq python3-argcomplete python3-colcon-common-extensions python-rosdep python3-vcstool
 sudo rosdep init
