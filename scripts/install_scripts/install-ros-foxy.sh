2e1f1c0e88628cebd92483d29943c1714aec20ac
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2021-01-24 20:51:09 -0500

    Added rosdep to foxy install

--- scripts/install_scripts/install-ros-foxy.sh
+++ scripts/install_scripts/install-ros-foxy.sh
@@ -8,14 +8,12 @@ sudo apt update && sudo apt install curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 
-sudo apt update -y
+sudo apt update
 sudo apt install -y ros-foxy-desktop
-source /opt/ros/foxy/setup.bash
 echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
-sudo apt install -y -qq python3-argcomplete
-sudo apt install -y -qq python3-colcon-common-extensions
-sudo apt-get -qq -y install python-rosdep
+source /opt/ros/foxy/setup.bash
+sudo apt install -y python3-argcomplete python3-colcon-common-extensions python3-rosdep python3-vcstool
 sudo rosdep init
 rosdep update
-sudo rosdep fix-permissions
+rosdep install --from-paths src --ignore-src -r -y
